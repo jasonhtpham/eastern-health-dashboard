11bed4a7b076fcf27fed4731bc21cdcd841ee462
Jason Pham <61170796+jasonhtpham@users.noreply.github.com>
2021-11-03 20:07:31 -0700

    Initial commit

---
+++ src/views/index.js
@@ -0,0 +1,19 @@
+/***
+ *  Created by Sanchit Dang
+ ***/
+import { Login } from './common/Login/Login';
+import { AuthCallback } from './common/Login/AuthCallback';
+import { FourOFour } from './common/FourOFour/FourOFour';
+import { Register } from './common/Register/Register';
+import { Home } from './dependants/Home/Home';
+import { MobileMenu } from './dependants/mobileMenu/mobileMenu';
+import { Example } from './dependants/Example/Example';
+export {
+  Example,
+  Login,
+  Register,
+  Home,
+  MobileMenu,
+  FourOFour,
+  AuthCallback
+};
